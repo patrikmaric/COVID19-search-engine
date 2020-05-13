9e5fe43f012935c90d9f18e63b5fcfeb284895e4
Nikolina Covic <nikolina.covic12@gmail.com>
2020-05-13 17:35:01 +0200

    Fix doc2vec

--- query_model/transformers/doc2vec.py
+++ query_model/transformers/doc2vec.py
@@ -11,8 +11,8 @@ import pandas as pd
 import numpy as np
 
 # Enters paragraph; make sentences and words to feed W2V
-#from data import CovidDataLoader
-from dataset.data import CovidDataLoader
+from data import CovidDataLoader
+#from dataset.data import CovidDataLoader
 from query_model.queries import QueryEngine
 
 from settings import data_root_path
@@ -46,12 +46,22 @@ class D2VQueryEngine(QueryEngine):
 
     def __build_d2v(self):
         tok_corpus = []
-        for paragraph in self.corpus['text']:
-            senten = sent_tokenize(paragraph)
+        tags = []
+        words = []
+        for i,paragraph in enumerate(self.corpus['text']):
+            tags += str(i)
+            sentn = sent_tokenize(paragraph)
+            par_words = []
+            for sent in sentn:
+                par_words += word_tokenize(word_stem(sent))
+            words.append(par_words)
+        tok_corpus+=[TaggedDocument(words=words[i], tags=tags[i]) for i in range(len(words))] ##CHECK
+            #senten = sent_tokenize(paragraph)
 #            for sent in senten:
 #                sent = word_stem(sent)
 #                tok_corpus.append(word_tokenize(sent))
-            tok_corpus+=[TaggedDocument(words=word_tokenize(word_stem(_d)), tags=[str(i)]) for i, _d in enumerate(senten)]
+        #tok_corpus+=[TaggedDocument(words=word_tokenize(word_stem(sent)), tags=[str(i)]) for i, _d in enumerate(paragraph) for sent in sent_tokenize(_d)] ##CHECK
+#        print(tok_corpus)
         # building vocab
         self.d2v = Doc2Vec(dm=0, vector_size=300, min_count=5, negative=5, hs=0, sample=0, epochs=40) #it was 2, but it says that it works better with min_count=5
         self.d2v.build_vocab(tok_corpus)
@@ -85,7 +95,7 @@ class D2VQueryEngine(QueryEngine):
 if __name__ == '__main__':
     article_paths = CovidDataLoader.load_articles_paths(data_root_path)
 
-    abstracts = CovidDataLoader.load_data(article_paths, offset=0, limit=30000, load_sentences=False, preprocess=False)
+    abstracts = CovidDataLoader.load_data(article_paths, offset=0, limit=10000, load_sentences=False, preprocess=False)
     query1 = word_stem("Main risk factors of covid19")
     query2 = word_stem("Does smoking increase risks when having covid19?")
     query3 = word_stem("What is the mortality rate of covid19?")
