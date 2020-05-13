7071058ee53b423f08e5f5e146545fb1fb9ce2c7
patrikmaric <pmari2112@gmail.com>
2020-05-13 14:36:24 +0200

    w2v v1.0

--- query_model/transformers/representations.py
+++ query_model/transformers/representations.py
@@ -15,7 +15,6 @@ import numpy as np
 from dataset.data import CovidDataLoader
 from query_model.queries import QueryEngine
 
-
 # train_corpus = whole corpus tokenized
 from settings import data_root_path
 
@@ -41,9 +40,15 @@ class W2VQueryEngine(QueryEngine):
 
     def run_query(self, query, n=5):
         # TODO; check this
-        query_vector = self.get_paragraph_embedding(query).reshape(1, -1)
-        similarities = cosine_similarity(self.paragraph_vectors,
-                                         query_vector)  # TODO: check if this already sorts values
+        query_vector = self.get_paragraph_embedding(query)
+
+        n1 = np.linalg.norm(query_vector)
+        qvn = np.divide(query_vector, n1)
+        # query_vector = np.linalg.norm(query_vector)
+        n2 = np.linalg.norm(self.paragraph_vectors, axis=1)
+        pvn = np.divide(self.paragraph_vectors, n2.reshape(-1, 1))
+
+        similarities = qvn.dot(pvn.T)
         return self.__create_query_result(query, similarities, n)
 
     def __build_w2v(self):
@@ -60,7 +65,7 @@ class W2VQueryEngine(QueryEngine):
         vectors = []
         for element in self.corpus['text']:
             element_vec = self.get_paragraph_embedding(element).reshape(1, -1)
-            vectors.append(element_vec)
+            vectors.append(element_vec[0])
 
         self.paragraph_vectors = np.array(vectors)
 
@@ -81,18 +86,30 @@ class W2VQueryEngine(QueryEngine):
                 result_vec += np.array(self.w2v[word])
         return result_vec
 
+    def __create_query_result(self, query, similarities, n):
+        result = {
+            'id': self.corpus['paper_id'],
+            'query': query,
+            'text': self.corpus['text'],
+            'sim': similarities,
+        }
+
+        result = pd.DataFrame(result).sort_values(by='sim', ascending=False)[:n]
+
+        return result[result['sim'] > 0]
+
 if __name__ == '__main__':
     article_paths = CovidDataLoader.load_articles_paths(data_root_path)
-    abstracts = CovidDataLoader.load_data(article_paths, offset=5000, limit=300, load_sentences=False, preprocess=False)
+    abstracts = CovidDataLoader.load_data(article_paths, offset=0, limit=30000, load_sentences=False, preprocess=False)
     # TODO: input
-    #abstracts_text = list(abstracts['text'])
+    # abstracts_text = list(abstracts['text'])
     query1 = word_stem("Main risk factors for covid19")
     query2 = word_stem("Does smoking increase risks when having covid19?")
     query3 = word_stem("What is the mortality rate of covid19?")
 
     query_engine = W2VQueryEngine()
     query_engine.fit(abstracts)
-
+    results = query_engine.run_query(query1)
 
     """model = build_model(abstracts_text)  # needs to send whole paragraphs
     print("Main risk factors for covid19")
